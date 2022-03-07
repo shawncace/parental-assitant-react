20783f1dfe7af8295208f99430042ebf98c2d611
shawn <shawncaceres10@gmail.com>
2022-03-07 08:23:47 -0600

    added functionality to toggle component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {DataProvider} from "./AppContext";
 import './stylesheets/App.css'
-import './stylesheets/DarkMode.css'
+
 import Header from "./components/Header";
 import Options from "./components/Options"
 import Footer from "./components/Footer"
@@ -9,7 +9,6 @@ function App() {
   return (
     <DataProvider>
       <div className="App">
-        yo
         <Header />
         <Options />
         <Footer />
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,12 +1,38 @@
+import '../stylesheets/DarkMode.css'
+
 export default function Toggle(){
+  let clickedClass = "clicked"
+  const body = document.body
+  const lightTheme = "light"
+  const darkTheme = "dark"
+  let theme
+
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme)
+} else {
+    body.classList.add(lightTheme)
+}
+
+  const switchTheme=(e)=>{
+    if(theme==='dark'){
+      body.classList.replace(darkTheme, lightTheme)
+      e.target.classList.remove(clickedClass)
+      theme='light'
+    }else{
+      body.classList.replace(lightTheme, darkTheme)
+      e.target.classList.add(clickedClass)
+      theme='dark'
+    }
+    console.log(theme)
+  }
+  
   return(
     <>
-      <div
-        // className = {theme==='dark'?clickedClass: ''}
-        id='darkMode'
-        // onClick={switchTheme}
-        >
-      </div>
+      <div 
+      className = {theme==='dark'?clickedClass: ''}
+      id='darkMode'
+      onClick={switchTheme}>
+    </div>
     </>
   )
 }
